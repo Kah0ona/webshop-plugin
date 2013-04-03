1d84a24168e0709ee234bde2db841ce98f6fe66c
Marten Sytema <marten@sytematic.nl>
2013-04-03 22:34:57 +0200

    wip

---
+++ models/TransactionResultModel.php
@@ -0,0 +1,12 @@
+<?php
+class TransactionResultModel extends GenericModel {
+
+	protected $options=null;
+
+	function __construct($options) {
+		$this->options=$options;
+	} 
+		
+	
+}
+?>
---
+++ views/TransactionResultView.php
@@ -0,0 +1,47 @@
+<?php 
+ //possible success url: 
+ //http://localhost/~marten/wordpress/success/?status=Success&trxid=TEST080486601601&ec=33&sha1=8ef02ea650c86184e75d491ef62a12dae7b61930
+ //	echo sha1("333349995253750745746a71baa15a76e3883e800867760234b18b2e04e");
+class TransactionResultView extends GenericView {
+	
+	protected $status=null;
+	protected $trxid=null;
+	protected $Order_id=null;
+	protected $sha1=null;
+	
+	public function render($data=null) { 
+		
+		$this->status = $_GET['status'];
+		$this->trxid = $_GET['trxid'];
+		$this->Order_id = $_GET['ec'];
+		$this->sha1 = $_GET['sha1'];
+		$msg = '';
+		if($this->status == 'Success'){
+			$msg = 'Uw bestelling is succesvol verwerkt en betaald.'; 
+			$class = 'success'; 
+		}
+		elseif($this->status == 'Cancelled'){
+			$msg = 'Uw bestelling is geannuleerd. '; 
+			$class = 'info';
+		}
+		elseif($this->status == 'Failure'){
+			$msg = 'Er is  iets misgegaan met de betaling. We hebben de bestelling opgeslagen, maar aangemerkt als onbetaald. Neem contact met ons op als u denkt dat dit niet klopt.'; 
+			$class = 'error';
+		}
+		elseif($this->status == 'Expired'){
+			$msg = 'Uw sessie is verlopen. De betaling is niet gelukt. We hebben uw bestelling wel opgeslagen, maar aangemerkt als onbetaald. Neem contact met ons op als u denkt dat dit niet klopt.'; 
+			$class = 'error';
+		}
+		else {
+			$class = 'info';
+		}
+						
+		?>
+		<div class='alert alert-<?php echo $class; ?> result_message'><?php echo $msg; ?></div>
+		<?
+		
+
+
+	}
+}
+?>
